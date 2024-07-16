9a712401dfdf19412cc1a46f763d83e16d8b3d38
Joss2309 <155509519+Joss2309@users.noreply.github.com>
2024-07-15 22:23:54 -0600

    Update Ejercicio4_Consulta_API.py

--- Ejercicio4_Consulta_API.py
+++ Ejercicio4_Consulta_API.py
@@ -1,14 +1,15 @@
 import json 
 import requests 
+from requests.auth import HTTPBasicAuth
 
 URL = "https://my.sportmonks.com/login"
 
 #Autenticación de crear usurario y contraseña
-usuario = 'JossMar'
+email = 'psarias23@gmail.com'
 contraseña = '230991'
 
 #Realizar la consulta GET a la autenticación con REQUESTS
-respuesta = requests.get(URL)
+respuesta = requests.get(URL, auth=HTTPBasicAuth(email, contraseña))
 
 if respuesta.status_code == 200:
     print('Solicitud exitosa')
