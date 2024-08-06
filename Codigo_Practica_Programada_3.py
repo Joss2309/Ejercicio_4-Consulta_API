b4dce5d91952bd272054b04be5af7cbb286403f3
Joss2309 <155509519+Joss2309@users.noreply.github.com>
2024-08-06 10:39:53 -0600

    Codigo_Practica_Programada_3.py

--- Codigo_Practica_Programada_3.py
+++ Codigo_Practica_Programada_3.py
@@ -14,14 +14,22 @@ from selenium.webdriver.common.by import By
 
 pagina_principal = "https://www.scrapethissite.com/pages/forms/"
 
-#Inicio busqueda en el navegador chrome, ingresar a la página del URL y dar un espera de 10 segundos para que los eleentos carguen
+#Inicio busqueda en el navegador chrome, ingreso a la página del URL 
+#solicito unaa espera de 10 segundos para que los elementos carguen
 navegador = webdriver.Chrome()
 navegador.get(pagina_principal)  # Abre la página web especificada por la URL.
 navegador.implicitly_wait(10) 
 
+# Le indico una lista vacía de datos que luego me va a almacenar la información que deseo.
+# Lo que investigue la segunda línea 27 le indico que los datos equipo, desde el navegador 
+# controlado por Selenium, realice la busqueda de los datos de la pagina  web, donde le especifico 
+# que me busque el nombre de clase CSS. y yo desde la pagina averigue era team, para que me los seleccione y 
+# me los forme en una lista.
 datos = []
 equipos = navegador.find_elements(By.CLASS_NAME, 'team')
 
+# El for entendí es un bucle que repite todas las instrucciones que le indico sobre cada lista de equipos,
+# y este dentro de equipos, con la clase css y me  extraiga  la información que realice como lista de dicionario.
 for equipo in equipos:
     nombre = equipo.find_element(By.CLASS_NAME, "name").text
     anio = equipo.find_element(By.CLASS_NAME, "year").text
@@ -32,7 +40,9 @@ for equipo in equipos:
     goles_favor = equipo.find_element(By.CLASS_NAME, "gf").text
     goles_contra = equipo.find_element(By.CLASS_NAME, "ga").text
     gol_diferencia = equipo.find_element(By.CLASS_NAME, "diff").text
-    
+
+# Utilice el append para añadir un la lista que ya cree y me construya cada elemento
+# para extraer información de la web, la información que deseo en una lista.    
     datos.append({
         'NOMBRE' : nombre,
         'AÑO' : anio,
